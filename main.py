bbd22c3b281998983086543c681d84785bcaf9b0
aanchaltailwal <tailwalaanchal@gmmail.com>
2023-08-13 20:18:42 +0530

    final commit

--- main.py
+++ main.py
@@ -1,24 +1,71 @@
+def sum(a, b, c):
+    return a + b + c
+
 def printBoard(xState, zState):
-    print(f"0 | 1 | 2 ")
+    zero = 'X' if xState[0] else ('0' if zState[0] else 0)
+    one = 'X' if xState[1] else ('0' if zState[1] else 1)
+    two = 'X' if xState[2] else ('0' if zState[2] else 2)
+    three = 'X' if xState[3] else ('0' if zState[3] else 3)
+    four = 'X' if xState[4] else ('0' if zState[4] else 4)
+    five = 'X' if xState[5] else ('0' if zState[5] else 5)
+    six = 'X' if xState[6] else ('0' if zState[6] else 6)
+    seven = 'X' if xState[7] else ('0' if zState[7] else 7)
+    eight = 'X' if xState[8] else ('0' if zState[8] else 8)
+    print(f"{zero}| {one} | {two} ")
     print(f"--|---|---")
-    print(f"3 | 4 | 5 ")
+    print(f"{three} | {four} | {five} ")
     print(f"--|---|---")
-    print(f"6 | 7 | 8 ")
-    
+    print(f"{six} | {seven} | {eight} ")
+
+def checkWin(xState, zState):
+    wins = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]]
+    for win in wins:
+        if(sum(xState[win[0]], xState[win[1]], xState[win[2]]) == 3):
+            print("X Won the match")
+            return 1
+        if(sum(zState[win[0]], zState[win[1]], zState[win[2]]) == 3):
+            print("0 Won the match")
+            return 0
+    return -1
+
+def restartGame():
+    xState = [0, 0, 0, 0, 0, 0, 0, 0, 0]
+    zState = [0, 0, 0, 0, 0, 0, 0, 0, 0]
+    return xState, zState
 
 if __name__ == "__main__":
-    xState = [0, 0, 0, 0, 0, 0, 0, 0]
-    yState = [0, 0, 0, 0, 0, 0, 0, 0]
-    turn = 1 # 1 for x and 0 for 0
-    print("Welcome to Tic Tac Toe")
-    while(True):
-        printBoard(xState, zState)
-        if(turn == 1):
-            print("X's Chance")
-            value = int(input("Please enter a value"))
-            xState[value] = 1
-        else:
-            print("X's Chance")
-            value = input("Please enter a value")
-            zState[value] = 1
-        break
+    while True:  # Outer loop for restarting the game
+        xState, zState = restartGame()  # Initialize the game states
+        turn = 1  # 1 for x and 0 for 0
+        print("Welcome to Tic Tac Toe")
+
+        while True:  # Inner loop for a single game
+            printBoard(xState, zState)
+
+            if turn == 1:
+                print("X's Chance")
+            else:
+                print("0's Chance")
+            
+            value = int(input("Please enter a value: "))
+            
+            if turn == 1:
+                xState[value] = 1
+            else:
+                zState[value] = 1
+            
+            cwin = checkWin(xState, zState)
+            if cwin != -1:
+                printBoard(xState, zState)
+                if cwin == 1:
+                    print("X Won the match")
+                else:
+                    print("0 Won the match")
+                break  # Exit the inner loop when the match is over
+            
+            turn = 1 - turn
+        
+        restart_choice = input("Do you want to play again? (yes/no): ")
+        if restart_choice.lower() != "yes":
+            print("Thanks for playing!")
+            break  # Exit the outer loop if the players don't want to continue
